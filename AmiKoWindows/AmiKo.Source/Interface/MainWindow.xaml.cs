8ff5a061087be63fc503c00d03128ddff05c7da7
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2018-05-11 16:28:02 +0900

    Move xaml and interface into Source

--- AmiKoWindows/AmiKo.Source/Interface/MainWindow.xaml.cs
+++ AmiKoWindows/AmiKo.Source/Interface/MainWindow.xaml.cs
@@ -235,12 +235,7 @@ namespace AmiKoWindows
 
         private async void MainWindowLoaded(object sender, RoutedEventArgs e)
         {
-            /*
-            Uri iconUri = new Uri("../Resources/img/desitin_icon.ico", UriKind.RelativeOrAbsolute); //make sure your path is correct, and the icon set as Resource
-            this.Icon = BitmapFrame.Create(iconUri, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.OnLoad);
-            */
             _statusBarHelper.IsConnectedToInternet();
-            //
             await _sqlDb?.Search(_uiState, "");
         }
 
