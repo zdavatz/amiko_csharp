4c29bbf6308916da7b36d68bba234713dafd639c
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2018-05-11 16:32:18 +0900

    Remove unused file (and `htmls` directory)

--- AmiKoWindows/PostBuild.bat
+++ AmiKoWindows/PostBuild.bat
@@ -7,6 +7,5 @@ ECHO --- Copying important files and folder ---
 (ROBOCOPY "%1\AmiKoWindows\Resources\img" "%2\Resources\img" /NP /NJH)
 
 (ROBOCOPY "%1\AmiKoWindows\dbs\%3" "%2\dbs" /NP /NJH)
-(ROBOCOPY "%1\AmiKoWindows\htmls" "%2\htmls" /NP /NJH) ^& IF %ERRORLEVEL% LEQ 1 exit 0
 
 (COPY /Y "%1\AmiKoWindows\store\%3\appx.manifest" "%2\AppxManifest.xml")
