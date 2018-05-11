116a93c6c0ee4d9bc0adc507f4040a5dfd71ae2a
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2018-05-11 20:26:02 +0900

    Make to run clean target separately

--- AmiKoWindows/PreBuild.bat
+++ AmiKoWindows/PreBuild.bat
@@ -1,6 +1,12 @@
 REM Make sure this file is encoded as UTF-8 (no BOM)
 
-ECHO --- Copying important files and folder ---
+SET resxfile="%1\AmiKoWindows\Properties\Resources.resx"
+IF EXIST %resxfile% (
+  ECHO --- Deleting resource file ---
+  DEL %resxfile%
+)
+
+ECHO --- Copying resource file ---
 
 :: copy {de|fr}.resx to resx
 (COPY /Y "%1\AmiKoWindows\Properties\%2" "%1\AmiKoWindows\Properties\Resources.resx")
