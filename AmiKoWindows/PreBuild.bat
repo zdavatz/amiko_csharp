38e3b13e831b46e8c9ea4ef4aa74ac8e6a57719a
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2018-05-11 15:52:02 +0900

    Rename properties

--- AmiKoWindows/PreBuild.bat
+++ AmiKoWindows/PreBuild.bat
@@ -3,4 +3,4 @@ REM Make sure this file is encoded as UTF-8 (no BOM)
 ECHO --- Copying important files and folder ---
 
 :: copy {de|fr}.resx to resx
-(COPY /Y "%1\AmiKoWindows\properties\%2" "%1\AmiKoWindows\properties\Resources.resx")
+(COPY /Y "%1\AmiKoWindows\Properties\%2" "%1\AmiKoWindows\Properties\Resources.resx")
