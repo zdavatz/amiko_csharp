c9905774167bcb70bd3b96b0dae5f61b7ad64ad2
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2018-05-28 15:17:43 +0900

    Setup unit tests (separate projects) using NUnit 3

---
+++ AmiKoWindows.Tests/src/UtilityTest.cs
@@ -0,0 +1,18 @@
+using System.IO;
+using NUnit.Framework;
+
+using AmiKoWindows;
+
+
+namespace AmiKoWindows.Tests
+{
+    [TestFixture]
+    public class UtilityTest
+    {
+        [Test]
+        public void Test_One()
+        {
+            Assert.IsTrue(1 == 1, ":'(");
+        }
+    }
+}
