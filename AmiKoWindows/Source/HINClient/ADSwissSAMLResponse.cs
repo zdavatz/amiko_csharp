bc16fad694d1464397e3faeda2c7d3670b17bab7
b123400 <i@b123400.net>
2023-09-10 22:05:48 +0900

    Fetch ADSwiss SAML

---
+++ AmiKoWindows/Source/HINClient/ADSwissSAMLResponse.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmiKoWindows.Source.HINClient
+{
+    public class ADSwissSAMLResponse
+    {
+        public string URL;
+        public string EPDAutURL;
+
+        public static ADSwissSAMLResponse FromResponseJSON(string jsonStr)
+        {
+            ADSwissSAMLResponseJSONPresenter o = JsonConvert.DeserializeObject<ADSwissSAMLResponseJSONPresenter>(jsonStr);
+            var r = new ADSwissSAMLResponse();
+            r.URL = o.url;
+            r.EPDAutURL = o.epdAuthUrl;
+            return r;
+        }
+    }
+    internal class ADSwissSAMLResponseJSONPresenter
+    {
+        public string url;
+        public string epdAuthUrl;
+    }
+}
--- AmiKoWindows/Source/HINClient/HINClient.cs
+++ AmiKoWindows/Source/HINClient/HINClient.cs
@@ -8,9 +8,11 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Json;
 using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace AmiKoWindows.Source.HINClient
 {
@@ -49,6 +51,24 @@ namespace AmiKoWindows.Source.HINClient
                 ADSwissApplicationName
                 );
         }
+        static public string HINDomainForADSwiss()
+        {
+#if DEBUG
+            return "oauth2.ci-prep.adswiss.hin.ch";
+#else
+            return "oauth2.ci.adswiss.hin.ch";
+#endif
+        }
+
+        static public string CertifactionDomain()
+        {
+#if DEBUG
+            return HINClientCredentials.CertifactionTestServer;
+#else
+            return HINClientCredentials.CertifactionServer;
+#endif
+        }
+
         static public string OAuthCallbackURL()
         {
             return "http://localhost:23822/callback";
@@ -146,6 +166,27 @@ namespace AmiKoWindows.Source.HINClient
             var responseStr = await result.Content.ReadAsStringAsync();
             SDSProfileResponse profile = SDSProfileResponse.FromResponseJSON(responseStr);
             return profile;
+        }
+
+        static public async Task<ADSwissSAMLResponse> FetchADSwissSAML(OAuthTokens token)
+        {
+            token = await RenewTokensIfNeeded(token);
+            var client = new HttpClient();
+            var endpoint = String.Format("https://{0}/authService/EPDAuth?targetUrl={1}&style=redirect", HINDomainForADSwiss(), OAuthCallbackURL());
+            var request = new HttpRequestMessage
+            {
+                RequestUri = new Uri(endpoint),
+                Method = HttpMethod.Post,
+                Headers = {
+                    { HttpRequestHeader.Accept.ToString(), "application/json" },
+                    { HttpRequestHeader.Authorization.ToString(), $"Bearer {token.AccessToken}" },
+                }
+            };
+
+            var result = await client.SendAsync(request);
+            var responseStr = await result.Content.ReadAsStringAsync();
+            ADSwissSAMLResponse saml = ADSwissSAMLResponse.FromResponseJSON(responseStr);
+            return saml;
         }
     }
 }
